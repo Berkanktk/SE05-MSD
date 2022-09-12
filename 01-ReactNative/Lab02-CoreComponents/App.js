d2404c66c66916dc35f2206e59bc929918d85741
Berkanktk <berkank@live.dk>
2022-09-12 23:50:05 +0200

    Changed background color

--- 01-ReactNative/Lab02-CoreComponents/App.js
+++ 01-ReactNative/Lab02-CoreComponents/App.js
@@ -18,8 +18,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#393939',
     justifyContent: 'center',
   },
 });
